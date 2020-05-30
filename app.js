373e3cad5429a90ce362f8e802b4095baab6b005
Ayan Banerjee <ayanb07@gmail.com>
2020-05-31 01:24:15 +0530

    HUssey api decoupling

--- app.js
+++ app.js
@@ -8,21 +8,42 @@ const app = express();
 app.use(cors());
 
 // parse request of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static('uploads'));
+
+
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
 
 // define a simple route
-app.post('/ocr',(req,res)=>{
+app.post('/ocr', (req, res) => {
     let image_path = req.body["image_path"];
-    ocr.imageToDraftContent(image_path).then(function(content){
-        res.json(content);
-    }).catch(err=>{
+    ocr.imageToDraftContent(image_path).then(function (content) {
+        res.json(content["content"]);
+    }).catch(err => {
         console.error(err);
-        res.status(500).send({"error":"Server Error"});
+        res.status(500).send({
+            "error": "Server Error"
+        });
     });
 });
 
-app.listen(8080, ()=>{ 
-	console.log("Server is listening on port 8080"); 
+// define a simple route
+app.post('/rects', (req, res) => {
+    let image_path = req.body["image_path"];
+    ocr.imageToDraftContent(image_path).then(function (content) {
+        res.json(content["rectangles"]);
+    }).catch(err => {
+        console.error(err);
+        res.status(500).send({
+            "error": "Server Error"
+        });
+    });
+});
+
+
+app.listen(8080, () => {
+    console.log("Server is listening on port 8080");
 });
